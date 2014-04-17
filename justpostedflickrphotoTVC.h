4c9863e20ef3e8cdbe00cf3699d1240d8768117c
YuJianSirius <YuJianSirius@aol.com>
2014-04-17 14:23:02 +0800

    Shutterbug
    A app is about displaying pictures Asynchronously download from the
    internet (studying at the Stanford IOS7 vedio)

---
+++ justpostedflickrphotoTVC.h
@@ -0,0 +1,13 @@
+//
+//  justpostedflickrphotoTVC.h
+//  shutterbug
+//
+//  Created by xiaojia on 14-3-6.
+//  Copyright (c) 2014年 xiaojia. All rights reserved.
+//
+
+#import "FlickrphotoTVC.h"
+
+@interface justpostedflickrphotoTVC : FlickrphotoTVC
+
+@end
