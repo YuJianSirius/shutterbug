6198178ca0d9199d7ac8f53d20bf1d179fc8ccc1
YuJianSirius <YuJianSirius@aol.com>
2014-04-17 15:22:24 +0800

    commit

---
+++ shutterbug/Flickr Fetcher/FlickrAPIKey.h
@@ -0,0 +1,11 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Fall 2013.
+//  Copyright 2013 Stanford University. All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+
+#define FlickrAPIKey @"2182b522e9064a9c153cb3eaeea2dd75"
